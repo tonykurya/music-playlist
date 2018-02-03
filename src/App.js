600d210708a79d16f8d8fd69e2e027097a68e9e0
tonykurya <tonykurya@gmail.com>
2018-02-03 10:09:38 +0300

    Add inline-block to Playlist Component

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class Filter extends Component {
 class Playlist extends Component {
   render() {
     return (
-      <div style={{...defaultStyle, display: 'block-inline', width: "25%"}}>
+      <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
         <img/>
         <h3>Playlist Name</h3>
         <ul><li>Song 1</li><li>Song 2</li><li>Song 3</li></ul>
